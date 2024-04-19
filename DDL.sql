60352eb12336e614ced764c3d03520b3b4a052c3
heesanggod <sangin583@gmail.com>
2024-04-19 16:49:34 +0900

    DB 로직 구상

--- DDL.sql
+++ DDL.sql
@@ -48,4 +48,30 @@
 -- 이메일 인증번호 테이블에 등록된 이메일만 사용자의 이메일로 사용할 수 있음 (1 : 1)
 # 사용자 - 게시물 : 
 -- 사용자가 게시물을 작성할 수 있음 (1 : n)
-#  
+
+# ================================================================== #
+
+# >> 사용자 (아이디, 비밀번호, 이메일, 권한, 가입경로)
+# table name : user
+# user_id : VARCHAR(50) PRIMARY KEY
+# user_password : VARCHAR(255) NN
+-- (+ DB에는 암호화 형태로 저장되기때문에 255)
+# user_email : VARCHAR(100) NN UQ FK email_auth_number(email)
+# user_role : VARCHAR(15) NN DEFAULT('ROLE_USER') CHECK('ROLE_USER', 'ROLE_ADMIN')
+# join_path : VARCHAR(5) NN DEFAULT('HOME') CHECK('HOME', 'KAKAO', 'NAVER')
+
+# >> 이메일 인증번호 (이메일, 인증번호)
+# table name : email_auth_number
+# email : VARCHAR(100) PK
+# auth_number : VARCHAR(4) NN
+
+# >> 게시물 (접수번호, 상태, 제목,  내용, 작성자, 작성일, 조회수, 답변)
+# table name : board
+# reception_number : INT PK AI
+# status : BOOLEAN NN DEFAULT(false)
+# title : VARCHAR(100) NN
+# contents : TEXT NN 
+# writer_id : VARCHAR(50) NN FK user(user_id)
+# writer_datetime : DATETIME NN DEFAULT(now())
+# view_count : INT NN DEFAULT(0)
+# comment : TEXT 
