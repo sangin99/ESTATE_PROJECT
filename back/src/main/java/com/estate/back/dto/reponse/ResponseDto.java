a696078cc729124f87f7c57aaf8ca003699ba32a
heesanggod <sangin583@gmail.com>
2024-04-24 16:51:28 +0900

    Request / Response auth 작업

--- back/src/main/java/com/estate/back/dto/reponse/ResponseDto.java
+++ back/src/main/java/com/estate/back/dto/reponse/ResponseDto.java
@@ -1,7 +1,6 @@
 package com.estate.back.dto.reponse;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 
 import lombok.AllArgsConstructor;
