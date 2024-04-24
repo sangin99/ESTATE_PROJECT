ac593dac14758b311ffede3f0d737a6ffb1bfc90
heesanggod <sangin583@gmail.com>
2024-04-24 15:44:23 +0900

    API 기능세부 : 아이디 중복확인

---
+++ back/src/main/java/com/estate/back/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.estate.back.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.estate.back.dto.reponse.ResponseDto;
+import com.estate.back.dto.request.auth.IdCheckRequestDto;
+import com.estate.back.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+// Auth 모듈 컨트롤러 : /api/v1/auth
+
+@RestController   // JSON 형태의 리스폰스를 반환할 때 사용
+@RequestMapping ("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    
+    @PostMapping("/id-check")
+    public ResponseEntity<ResponseDto> idCheck (
+        @RequestBody @Valid
+        IdCheckRequestDto requestBody
+    ) {
+        ResponseEntity<ResponseDto> response = authService.idCheck(requestBody);
+        return response;
+    }
+
+}
