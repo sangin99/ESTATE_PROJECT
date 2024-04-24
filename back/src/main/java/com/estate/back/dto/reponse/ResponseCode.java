721a3b9845036fecf517e5aca559beb3299b8f0b
heesanggod <sangin583@gmail.com>
2024-04-24 12:56:08 +0900

    공통 상수, 메서드, 클래스 작성

---
+++ back/src/main/java/com/estate/back/dto/reponse/ResponseCode.java
@@ -0,0 +1,6 @@
+package com.estate.back.dto.reponse;
+
+// Response 의 공통된 Code 값
+public interface ResponseCode {
+    
+}
---
+++ back/src/main/java/com/estate/back/dto/reponse/ResponseDto.java
@@ -0,0 +1,6 @@
+package com.estate.back.dto.reponse;
+
+// Response 의 공통 형태
+public class ResponseDto {
+    
+}
