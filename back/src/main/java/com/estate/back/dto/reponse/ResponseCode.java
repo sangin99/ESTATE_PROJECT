9d99b4653d799c9bd64333f423a328849caf3d93
heesanggod <sangin583@gmail.com>
2024-04-24 15:08:34 +0900

    상태별 코드/메세지 출력 작업

--- back/src/main/java/com/estate/back/dto/reponse/ResponseCode.java
+++ back/src/main/java/com/estate/back/dto/reponse/ResponseCode.java
@@ -1,6 +1,24 @@
 package com.estate.back.dto.reponse;
 
+// 200 성공 : SU / Success.
+// 400 필수 데이터 미입력 : VF / Validation Failed.
+// 400 중복된 아이디 : DI / Duplicated Id.
+// 400 중복된 이메일 : DE / Duplicated Email.
+// 401 로그인 정보 불일치 : SF / Sign in Failed.
+// 401 인증 실패 : AF / Authentication Failed.
+// 500 토큰 생성 실패 : TF / Token creation Failed.
+// 500 이메일 전송 실패 : MF / Mail send  Failed.
+// 500 데이터베이스 오류 : DBE / Database Error.
+
 // Response 의 공통된 Code 값
 public interface ResponseCode {
-    
+    String SUCCESS = "SU";
+    String VALIDATION_FAILED = "VF";
+    String DUPLICATED_ID = "DI";
+    String DUPLICATED_EMAIL = "DE";
+    String SIGN_IN_FAILED = "SF";
+    String AUTHENTICATION_FAILED = "AF";
+    String TOKEN_CREATION_FAILED = "TF";
+    String MAIL_SEND_FAILED = "MF";
+    String DATABASE_ERROR = "DBE";
 }
