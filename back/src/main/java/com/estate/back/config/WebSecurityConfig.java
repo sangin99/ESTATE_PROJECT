70390303736c7c2929f34a4c97cf7997fe57cc53
heesanggod <sangin583@gmail.com>
2024-04-24 10:16:14 +0900

    Spring Web Security 설정

---
+++ back/src/main/java/com/estate/back/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.estate.back.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+// Spring Web Security 설정
+// - Basic 인증 미사용
+// - CSRF 정책 미사용
+// - Session 생성 정책 미사용
+// - CORS 정책 (모든 출처 - 모든 메서드 - 모든 패턴 허용)
+@Configurable
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+            .httpBasic(HttpBasicConfigurer::disable)
+            .csrf(CsrfConfigurer::disable)
+            .sessionManagement(sessionManagement -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .cors(cors -> cors.configurationSource(corConfigurationSource())
+            );
+
+            return httpSecurity.build(); 
+        }
+
+    // Cors 설정
+    @Bean
+    protected CorsConfigurationSource corConfigurationSource() {
+
+        CorsConfiguration configuration = new CorConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+    }
+}
