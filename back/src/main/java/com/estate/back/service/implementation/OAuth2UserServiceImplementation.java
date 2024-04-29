333df1fc93e262b3a5d2b88c03d04bcfa0ff09df
heesanggod <sangin583@gmail.com>
2024-04-29 15:53:38 +0900

    실제 서비스 작업

--- back/src/main/java/com/estate/back/service/implementation/OAuth2UserServiceImplementation.java
+++ back/src/main/java/com/estate/back/service/implementation/OAuth2UserServiceImplementation.java
@@ -1,15 +1,29 @@
 package com.estate.back.service.implementation;
 
+import java.util.Map;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.estate.back.entity.EmailAuthNumberEntity;
+import com.estate.back.entity.UserEntity;
+import com.estate.back.repository.EmailAuthNumberRepository;
+import com.estate.back.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class OAuth2UserServiceImplementation extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+    private final EmailAuthNumberRepository emailAuthNumberRepository;
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     
     @Override
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -26,14 +40,43 @@ public class OAuth2UserServiceImplementation extends DefaultOAuth2UserService {
         //     exception.printStackTrace();
         // }
 
-        if (oauthClientName.equals("KAKAO")) {
+        String id = getId(oAuth2User, oauthClientName);    // 카카오, 네이버에서 지정하는 id
+        //ex) KAKAO_3458611654
+        //ex) NAVER_WrEPcdLrr0x
+        String userId = oauthClientName + "_" + id.substring(0, 10);     // 내가 지정하는 id
 
-        }
-        if (oauthClientName.equals("NAVER")) {
+        // userId 형태가 데이터에 존재하는지 검사
+        boolean isExistUser = userRepository.existsByUserId(userId);
+        if (!isExistUser) {
             
+            // ex) 3458611654@kakao.com
+            // ex) WrEPcdLrr0asdkLdhgawdb564RECS@naver.com
+            String email = id + "@" + oauthClientName.toLowerCase() + ".com";
+            String password = passwordEncoder.encode(id);
+
+            EmailAuthNumberEntity emailAuthNumberEntity = new EmailAuthNumberEntity(email, "0000");
+            emailAuthNumberRepository.save(emailAuthNumberEntity);
+
+            UserEntity userEntity = new UserEntity(userId, password, email, "ROLE_USER", oauthClientName);
+            userRepository.save(userEntity);
         }
 
         return oAuth2User;
+    }
+
+    private String getId(OAuth2User oAuth2User, String oauthClientName) {
+        String id = null;
+
+        if (oauthClientName.equals("KAKAO")) {
+            Long longId = (Long) oAuth2User.getAttributes().get("id");
+            id = longId.toString();
+        }
+        if (oauthClientName.equals("NAVER")) {
+            Map<String, String> response = (Map<String, String>) oAuth2User.getAttributes().get("response");
+            id = response.get("id");           
+        }
+
+        return id;
     }
 }
 
