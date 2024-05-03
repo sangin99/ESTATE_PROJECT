2b97a870fd8eb5004fa0e32142ef866b3e70b59b
heesanggod <sangin583@gmail.com>
2024-05-03 14:53:49 +0900

    Q&A 게시판 FRONT

--- front/src/views/service/qna/QnaList/index.tsx
+++ front/src/views/service/qna/QnaList/index.tsx
@@ -1,51 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
+import { useUserStore } from 'src/stores';
+import { useNavigate } from 'react-router';
+import { QNA_WRITE_ABSOLUTE_PATH } from 'src/constant';
 
 //                    component                    //
 export default function QnaList() {
+  //                    state                    //
+  const {loginUserRole} = useUserStore();
+
+  const [totalLength, setTotalLength] = useState<number>(0);
+  const [totalPage, setTotalPage] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [isToggleOn, setToggleOn] = useState<boolean>(false);
+
+  //                    function                    //
+  const navigator = useNavigate();
+
+  //                    event handler                    //
+  const onWriteButtonClickHandler = () => {
+    if (loginUserRole !== 'ROLE_USER') return;
+    navigator(QNA_WRITE_ABSOLUTE_PATH);
+  }; 
+
+  const onToggleClickHandler = () => {
+    if (loginUserRole !== 'ROLE_ADMIN') return;
+    setToggleOn(!isToggleOn);
+  };
 
   //                    render                    //
+  const toggleClass = isToggleOn ? 'toggle-active':'toggle'
   return (
-    <div>
-      <div>
-        <div>전체 <span>150건</span> | 페이지 <span>1/100</span></div>
-        <div>
-          <div>글쓰기</div>
-          <div>
-            <div></div>
-            <div>미완료 보기</div>
-          </div>
+    <div id='qna-list-wrapper'>
+      <div className='qna-list-top'>
+        <div className='qna-list-size-text'>전체 <span className='emphasis'>{totalLength}건</span> 
+        | 페이지 <span className='emphasis'>{currentPage}/{totalPage}</span></div>
+        <div className='qna-list-top-right'>
+          {loginUserRole === 'ROLE_USER' ? 
+          <div className='primary-button' onClick={onWriteButtonClickHandler}>글쓰기</div> :
+          <>
+          <div className={toggleClass} onClick={onToggleClickHandler}></div>
+          <div className='qna-list-top-admin-text'>미완료 보기</div>
+          </>
+          }
         </div>
       </div>
-      <div>
-        <div>
-          <div>접수번호</div>
-          <div>상태</div>
-          <div>제목</div>
-          <div>작성자</div>
-          <div>작성일</div>
-          <div>조회수</div>
+      <div className='qna-list-table'>
+        <div className='qna-list-table-th'>
+          <div className='qna-list-table-reception-number'>접수번호</div>
+          <div className='qna-list-table-status'>상태</div>
+          <div className='qna-list-table-title'>제목</div>
+          <div className='qna-list-table-write-id'>작성자</div>
+          <div className='qna-list-table-write-date'>작성일</div>
+          <div className='qna-list-table-view-count'>조회수</div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className='qna-list-table-tr'>
+        <div className='qna-list-table-reception-number'>접수번호</div>
+          <div className='qna-list-table-status'>
+            <div className='primary-badge'>접수</div>
+          </div>
+          <div className='qna-list-table-title' style={{ textAlign:'left' }}>제목</div>
+          <div className='qna-list-table-write-id'>작성자</div>
+          <div className='qna-list-table-write-date'>작성일</div>
+          <div className='qna-list-table-view-count'>조회수</div>
         </div>
       </div>
-      <div>
-        <div>
-          <div></div>
-          <div>1</div>
-          <div></div>
-        </div>
-        <div>
-          <div>
-            <input />
+      <div className='qna-list-bottom'>
+        <div style={{width: '299px'}}></div>
+        <div className='qna-list-page-nation'>
+          <div className='qna-list-page-left'></div>
+          <div className='qna-list-page-box'>
+            <div className='qna-list-page-active'>1</div>
+            <div className='qna-list-page'>1</div>
           </div>
-          <div>검색</div>
+          <div className='qna-list-page-right'></div>
+        </div>
+        <div className='qna-list-search-box'>
+          <div className='qna-list-search-input-box'>
+            <input className='qna-list-search-input' />
+          </div>
+          <div className='disable-button'>검색</div>
         </div>
       </div>
     </div>
