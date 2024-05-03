ce6a5c8099064adc134113944e4658de108b1f63
heesanggod <sangin583@gmail.com>
2024-05-03 11:06:13 +0900

    Q&A 게시판 구조 : front

--- front/src/views/service/qna/QnaList/index.tsx
+++ front/src/views/service/qna/QnaList/index.tsx
@@ -1,6 +1,53 @@
 import React from 'react'
 import './style.css'
 
+//                    component                    //
 export default function QnaList() {
-  return <div>QnaList</div>;
+
+  //                    render                    //
+  return (
+    <div>
+      <div>
+        <div>전체 <span>150건</span> | 페이지 <span>1/100</span></div>
+        <div>
+          <div>글쓰기</div>
+          <div>
+            <div></div>
+            <div>미완료 보기</div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>접수번호</div>
+          <div>상태</div>
+          <div>제목</div>
+          <div>작성자</div>
+          <div>작성일</div>
+          <div>조회수</div>
+        </div>
+        <div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div></div>
+          <div>1</div>
+          <div></div>
+        </div>
+        <div>
+          <div>
+            <input />
+          </div>
+          <div>검색</div>
+        </div>
+      </div>
+    </div>
+  );
 }
