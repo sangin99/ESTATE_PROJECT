47974318592b55bdef5fd71ac5a6325c0a6d23ce
heesanggod <sangin583@gmail.com>
2024-05-08 12:22:56 +0900

    게시물 "수정" 작업

--- front/src/views/service/qna/QnaUpdate/index.tsx
+++ front/src/views/service/qna/QnaUpdate/index.tsx
@@ -1,6 +1,100 @@
-import React from 'react'
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
 import './style.css'
+import { useUserStore } from 'src/stores';
+import { useCookies } from 'react-cookie';
+import { useNavigate, useParams } from 'react-router';
+import { getBoardRequest } from 'src/apis/board';
+import { GetBoardResponseDto } from 'src/apis/board/dto/response';
+import ResponseDto from 'src/apis/response.dto';
+import { QNA_LIST_ABSOLUTE_PATH } from 'src/constant';
 
+
+//                    component                    //
 export default function QnaUpdate() {
-  return <div>QnaUpdate</div>;
+  
+  //                    state                    //
+  const contentsRef = useRef<HTMLTextAreaElement | null>(null);
+  const { loginUserId } = useUserStore();
+  const { receptionNumber } = useParams();
+  const [cookies] = useCookies();
+  const [writerId, setWriterId] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [contents, setContents] = useState<string>('');
+
+  //                    function                    //
+  const navigator = useNavigate();
+
+  const getBoardResponse = (result: GetBoardResponseDto | ResponseDto | null) => {
+    const message =
+      !result ? '서버에 문제가 있습니다.' :
+      result.code === 'VF' ? '올바르지 않은 접수 번호입니다.' :
+      result.code === 'AF' ? '인증에 실패했습니다.' :
+      result.code === 'NB' ? '존재하지 않는 접수 번호입니다.' :
+      result.code === 'DBE' ? '서버에 문제가 있습니다.' :
+      '';
+    
+    if (!result || result.code !== 'SU') {
+      alert(message);
+      navigator(QNA_LIST_ABSOLUTE_PATH);
+      return;
+    }
+    const { writerId, title, contents } = result as GetBoardResponseDto;
+    if (writerId !== loginUserId) {
+      alert('권한이 없습니다.');
+      navigator(QNA_LIST_ABSOLUTE_PATH);
+      return;
+    }
+
+    setTitle(title);
+    setContents(contents);
+    setWriterId(writerId);
+      
+  };
+
+  //                    event handler                    //
+  const onTitleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      const title = event.target.value;
+      setTitle(title);
+  };
+
+  const onContentsChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
+      const contents = event.target.value;
+      if (contents.length > 1000) return;
+      setContents(contents);
+
+      if (!contentsRef.current) return;
+      contentsRef.current.style.height = 'auto';
+      contentsRef.current.style.height = `${contentsRef.current.scrollHeight}px`;
+  };
+
+  const onUpdateButtonClickHandler = () => {
+      if (!title || !contents) return;
+      if (!cookies.accessToken) return;
+
+  };
+
+  //                    effect                    //
+  let effectFlag = false;
+  useEffect(() => {
+    if (!receptionNumber || !cookies.accessToken) return;
+    if (effectFlag) return;
+    effectFlag = true;
+    getBoardRequest(receptionNumber, cookies.accessToken).then(getBoardResponse);
+  }, []);
+
+  //                    render                    //
+  return (
+      <div id="qna-write-wrapper">
+          <div className='qna-write-top'>
+              <div className='qna-write-title-box'>
+                  <input className='qna-write-title-input' placeholder='제목을 입력해주세요.' value={title} onChange={onTitleChangeHandler} />
+              </div>
+              <div className='primary-button' onClick={onUpdateButtonClickHandler}>수정</div>
+          </div>
+          <div className='qna-write-contents-box'>
+              <textarea ref={contentsRef} className='qna-write-contents-textarea' placeholder='내용을 입력해주세요. / 1000자' maxLength={1000} value={contents} onChange={onContentsChangeHandler} />
+          </div>
+      </div>
+  );
+
 }
