bc8436e1ecb29e0432b57b7cff5ec803a0b4ac1a
heesanggod <sangin583@gmail.com>
2024-05-08 15:51:57 +0900

    게시글 작성 페이지에 아무것도 입력 안해도 요청 보내짐 : 해결

--- front/src/views/service/qna/QnaWrite/index.tsx
+++ front/src/views/service/qna/QnaWrite/index.tsx
@@ -55,7 +55,7 @@ export default function QnaWrite() {
     };
 
     const onPostButtonClickHandler = () => {
-        if (!title || !contents) return;
+        if (!title.trim() || !contents.trim()) return;
         if (!cookies.accessToken) return;
 
         const requestBody: PostBoardRequestDto = { title, contents };
