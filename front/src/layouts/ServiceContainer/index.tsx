c068a5db453ec8700c7c0250c85181e49cb59076
heesanggod <sangin583@gmail.com>
2024-05-08 17:19:50 +0900

    게시글 새로고침

--- front/src/layouts/ServiceContainer/index.tsx
+++ front/src/layouts/ServiceContainer/index.tsx
@@ -54,13 +54,19 @@ function SideNavigation({ path }: Props) {
     const ratioClass = `side-navigation-item${path === '비율 계산' ? ' active' : ''}`;
     const qnaClass = `side-navigation-item${path === 'Q&A 게시판' ? ' active' : ''}`;
 
+    //                    state                    //
+    const { pathname } = useLocation();
+
     //                    function                    //
     const navigator = useNavigate();
 
     //                    event handler                    //
     const onLocalClickHandler = () => navigator(LOCAL_ABSOLUTE_PATH);
     const onRatioClickHandler = () => navigator(RATIO_ABSOLUTE_PATH);
-    const onQnaClickHandler = () => navigator(QNA_LIST_ABSOLUTE_PATH);
+    const onQnaClickHandler = () => {
+        if (pathname === QNA_LIST_ABSOLUTE_PATH) window.location.reload();   // window.location.reload() : 새로고침
+        else navigator(QNA_LIST_ABSOLUTE_PATH);
+    };
 
     //                    render                    //
     return (
